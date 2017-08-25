add27232ccc0bf5022ce0993882c2ab5344fabc9
Patrik Luukkonen <patrik@fiberkompaniet.se>
2017-08-25 12:30:43 +0200

    hash

--- Fiberkompaniet/App_Data/Models/models.generated.cs
+++ Fiberkompaniet/App_Data/Models/models.generated.cs
@@ -19,7 +19,7 @@ using Umbraco.ModelsBuilder;
 using Umbraco.ModelsBuilder.Umbraco;
 
 [assembly: PureLiveAssembly]
-[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "97506ea93cf947a4")]
+[assembly:ModelsBuilderAssembly(PureLive = true, SourceHash = "aaede3b9863bfc7c")]
 [assembly:System.Reflection.AssemblyVersion("0.0.0.1")]
 
 namespace Umbraco.Web.PublishedContentModels
